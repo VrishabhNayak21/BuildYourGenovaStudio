e91d95491024392e669d4935e83623059d2f936d
VrishabhNayak21 <168457518+VrishabhNayak21@users.noreply.github.com>
2025-08-04 22:13:24 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:5000";
+  const url = "https://buildyourgenovastudio-backend.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
@@ -141,3 +141,4 @@ const StoreContextProvider = (props) => {
 };
 
 export default StoreContextProvider;
+
